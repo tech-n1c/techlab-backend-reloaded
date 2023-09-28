10bb752939d1014c0365dd44d99be26b456fcf1c
tech-n1c <nicimaiwald@gmail.com>
2023-09-28 17:55:36 +0200

    Kleine Verbesserung der Anleitung.

--- src/main/kotlin/de/netzebw/techlabbackendreloaded/hello/HelloRestController.kt
+++ src/main/kotlin/de/netzebw/techlabbackendreloaded/hello/HelloRestController.kt
@@ -25,7 +25,7 @@ class HelloRestController {
     Eine Übersicht über die verfügbaren Methoden:
     
     "method": "GET", "path": "/", "description": "Diese Übersicht"
-    "method": "GET", "path": "/hello", "description": "Hier kannst du dich mit deinem Namen begrüßen lassen. Gebe dafür deinen Namen als Parameter an."
+    "method": "GET", "path": "/hello", "description": "Hier kannst du dich mit deinem Namen begrüßen lassen. Gebe dafür deinen Namen als Parameter "name" an."
     "method": "GET", "path": "/puzzles", "description": "Eine Übersicht über die verfügbaren Rätsel"
 }"""
 
